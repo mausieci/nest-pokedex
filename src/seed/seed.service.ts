206e53ea71f2498158ce2f3ca4b7e98b1f3c36f9
Mauricio Sierra Cifuentes <2376@holbertonschool.com>
2022-07-25 22:38:41 -0500

    create seed module and endpoint that send request to pokeapi.co, to charge DB data

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
+
+@Injectable()
+export class SeedService {
+  private readonly axios: AxiosInstance = axios;
+
+  async executeSeed() {
+    const urlPokeAip = 'https://pokeapi.co/api/v2/pokemon?limit=300';
+    const { data } = await axios.get<PokeResponse>(urlPokeAip);
+
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no: number = +segments[segments.length - 2];
+      console.log({ no, name });
+    });
+    return data.results;
+  }
+}
